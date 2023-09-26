7f3aa54baaa54ee052d8e4b7f104b7792f134d8f
WinterMeza <e1317233748@live.uleam.edu.ec>
2023-09-25 20:50:15 -0500

    update
    x

---
+++ administrator/core/app/view/help-view.php
@@ -0,0 +1,8 @@
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+
+<p>Tambien me puedes contactar por email a <b>dalember@gmail.com</b></p>
+</div>
+</div>
+</div>
---
+++ core/app/view/help-view.php
@@ -0,0 +1,8 @@
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<p class="text-muted text-center">Powered by <a  target="_blank">Dalember Gorozabel</a> &copy; 2023</p>
+<p class="text-muted text-center"> TECNOLOGIAS DE LA INFORMACIÓN EMERGENTES 9NOB</p>
+</div>
+</div>
+</div>
